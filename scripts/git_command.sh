0c9fecb7a4d52f8a6d01af4e141127ef189ee39f
ngocbnu213 <nguyen.bao.ngoc@vjpco.com>
2020-10-13 19:32:44 +0900

    Update script

--- scripts/git_command.sh
+++ scripts/git_command.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git --version
+cd /home/ec2-user
 git clone https://nbngoc-vjp:Tinhbuon%401@github.com/VJP01/VJP-Employee-system.git
 cd VJP-Employee-system
 git checkout -b feature/#751_jenkins
